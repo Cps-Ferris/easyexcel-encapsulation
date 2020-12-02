da2f1c48d6ae3ec6d570bd54be22968b3787882e
Cai Peishen <peishen.cai@foxmail.com>
2020-12-02 14:14:40 +0800

    初始化

---
+++ src/main/java/cn/cps/easyexcel/EasyExcelEncapsulationApplication.java
@@ -0,0 +1,12 @@
+package cn.cps.easyexcel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyExcelEncapsulationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyExcelEncapsulationApplication.class, args);
+	}
+}
